ec4d991bb6b91bbad4d82ae9df6121195ff84429
Alan Henry <alandavidhenry@gmail.com>
2023-09-02 11:35:20 +0100

    Inital setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EquipmentComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
